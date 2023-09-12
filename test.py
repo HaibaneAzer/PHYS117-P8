489264f8d1c7965703f67401623a9d1f2ba36800
Marcus Mardal <puk015@uib.no>
2023-09-12 22:19:34 +0200

    working reader

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
-import sys
-directory_path = "/Users/marcu/anaconda3/envs/phys117_py2/Lib/site-packages"
-sys.path.append(directory_path)
 from LHCO_reader import LHCO_reader
 
 events = LHCO_reader.Events(f_name="example.lhco")
-print (events)
+
+events.plot("electron", "PT")
+
+""" for event in events: """
+    
+    
