5abe145e49c42059ef2191a553d88d3545fe9b95
Marcus Mardal <puk015@uib.no>
2023-09-25 20:01:57 +0200

    updated plotter

--- LHCO_plotter.py
+++ LHCO_plotter.py
@@ -1,6 +1,5 @@
 from LHCO_reader import LHCO_reader
 import numpy as np
-import collections
 import matplotlib.pyplot as plt
 import os
 
@@ -11,9 +10,6 @@ def read_events_from_file(file_path):
     except Exception as e:
         print("Error reading file: {}".format(e))
         return None
-    
-
-    
 
 def validate_events(events):
     if not isinstance(events, list):
@@ -23,70 +19,88 @@ def validate_events(events):
             raise ValueError("Each event must be a dictionary")
         # Additional validation checks for event data
 
-def LHCO_plot(events):
+def LHCO_plot(events, file, x_data, y_data):
     data = events.column('jet', 'PT')  # Make data into a column
 
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    y, binEdges = np.histogram(data, bins=100)
+    # create x/y-coordinates using data
+    y, binEdges = np.histogram(data, bins=50) # number of x-axis-segments 
     bincenters = 0.5*(binEdges[1:]+binEdges[:-1])
-    ax.plot(bincenters, y, '-')
-    """ ax.hist(data, 50, normed=1, facecolor='Crimson', alpha=0.9) """
-    ax.grid()  # Add grid lines
 
-    ax.set_title('jet PT')  # Titles etc
-    ax.set_xlabel('PT')
-    ax.set_ylabel("Frequency")
-    plt.rc('font', size=20)  # Fonts - LaTeX possible, but slow
+    # normalize y
+    normalized_y = y / np.sum(data)
 
-    plt.show()  # Show the plot on screen
+    x_data.append(bincenters)
+    y_data.append(normalized_y)
 
-def main(idx, jdx):
+    return file
+
+def main():
     # define path and file name
     data_path = os.getcwd()
 
+    # get list of all subdirectories in the data directory
     subdirectories = next(os.walk(data_path))[1]
     subdirectories.remove('.git') # ignore git folder
 
-    # error check
-    if idx >= len(subdirectories):
-        raise ValueError("Invalid idx value. There is only {} subdirectories".format(len(subdirectories)))
-    
-    folder_name = subdirectories[idx]
-    folder_path = os.path.join(data_path, folder_name)
+    # Prompt user to select a subdirectory
+    print("Select a subdirectory:")
+    for idx, subdir in enumerate(subdirectories):
+        print("{}. {}".format(idx+1 , subdir))
 
-    file_list = os.listdir(folder_path)
+    subdir_idx = int(input("Enter the index of the subdirectory: ")) - 1
+    selected_subdir = subdirectories[subdir_idx]
 
-    # error check
-    if jdx >= len(file_list):
-        raise ValueError("Invalid jdx value. There are only {} files in {}".format(len(file_list), folder_name))
+    # List files in the selected subdirectory
+    subdir_path = os.path.join(data_path, selected_subdir)
+    file_list = os.listdir(subdir_path)
 
-    file_path = os.path.join(data_path, folder_name, file_list[jdx])
-    """ print("filepath:", file_path) # current path """
+    # Prompt user to select files
+    print("\nSelect files from {}:".format(selected_subdir))
+    for idx, file_name in enumerate(file_list):
+        print("{}. {}".format(idx+1, file_name))
 
-    print("file:", str(file_list[jdx])) # current file
+    selected_files = raw_input("Enter the indices of the files (comma-separated): ") # 2.x python only
+    selected_files = selected_files.split(',')
+    selected_files = [int(idx.strip()) - 1 for idx in selected_files]
 
-    # read and convert file to event object
-    print()
-    events = read_events_from_file(file_path)
-    print()
-    
-    if events is not None:
-        # validate events
-        validate_events(events)
+    # initialize lists to store data
+    x_data = []
+    y_data = []
 
-        # plot
-        LHCO_plot(events)
+    legend_labels = []
+
+    # Process selected files
+    for idx in selected_files:
+        if 0 <= idx < len(file_list):
+            file_name = file_list[idx]
+            file_path = os.path.join(subdir_path, file_name)
+
+            # Read and convert file to event object
+            events = read_events_from_file(file_path)
+
+            if events is not None:
+                # Validate events
+                validate_events(events)
+
+                # Plot
+                legend_label = LHCO_plot(events, file_name, x_data, y_data)
+                legend_labels.append(legend_label)
+
+    # plot all data into one graph
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    for x, y, label in zip(x_data, y_data, legend_labels):
+        ax.plot(x, y, '-', label=label)
         
+    ax.grid()
+    ax.set_title('jet PT')
+    ax.set_xlabel('PT (GeV)')
+    ax.set_ylabel('Relative Frequency')
+    ax.legend()
+
+    plt.show()
 
 # run
 if __name__ == "__main__":
-    print()
-    folder = int(input("folder (int): "))
-    file = int(input("file (int): "))
-    print()
-    main(folder, file)
-
-    
-
-    
+    main()
